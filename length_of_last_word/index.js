c407d0d05cd16498952f4533bc009d7dacc2cb82
Samuel May <sam.may.2412@gmail.com>
2020-06-09 15:55:53 +0100

    Added links to challenge

--- length_of_last_word/index.js
+++ length_of_last_word/index.js
@@ -1,3 +1,7 @@
+// LEETCODE Challenge - Length of Last Word
+// https://leetcode.com/problems/length-of-last-word/
+// Completed 2020/06/09
+
 /**
  * @param {string} s
  * @return {number}
@@ -6,5 +10,3 @@ var lengthOfLastWord = function(s) {
   let words = s.trim().split(' ');
   return words[words.length-1].length;
 }
-
-console.log(lengthOfLastWord("a "));
